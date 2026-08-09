6610eb9ddbca0066952a9172ed482a06da759cf8
fathwi <fathwi882@gmail.com>
2026-08-09 21:00:13 +0300

    its me again fahtwi

---
+++ evento50fathwi.py
@@ -0,0 +1,11 @@
+
+listofnum = []
+for i in range(0,51):
+    if i % 2 == 0 :
+        print(i)
+        listofnum.append(i)
+    else:
+        continue
+
+print(f"this is fatwi to 50 {listofnum}")
+
